45ac980b74982bde696e94339c583e6fd0cb61c8
HectorERS <hector.rios@mail.udp.cl>
2022-11-09 19:21:25 -0300

    Backend tienda, plantilla base, con conexion a vistas

--- src/routes/Productos.js
+++ src/routes/Productos.js
@@ -2,8 +2,24 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../database');
 
-router.get('/productos', (req, res) => {
-    res.render('index');
+
+//trae todos los productos con su categoria
+router.get('/productos', async (req, res) => {
+    const productos = await pool.query('SELECT *, category.name as category_name FROM product INNER JOIN category ON product.category = category.id');
+    res.json(productos);
+})
+
+//filtrar productos por el id del producto
+router.get('/productos/:id', async(req, res) => {
+   const { id } = req.params;
+   const productos = await pool.query('SELECT * FROM product WHERE product.id = ?', [id]);
+   res.json(productos)
+})
+
+router.get('/productos/categoria/:categoria', async(req, res) => {
+    const { categoria } = req.params;
+    const category = await pool.query('SELECT product.name, product.price, product.url_image, product.discount, category.name as category_name FROM product, category where category.id = product.category and category.name = ?', [categoria]);
+    res.json(category);
 })
 
 module.exports = router;
